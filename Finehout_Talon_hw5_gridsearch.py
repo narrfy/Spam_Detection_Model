c3cabaaea5f2113a9f4287145f14440c0b9c57bb
narrfy <146122385+narrfy@users.noreply.github.com>
2024-04-18 18:31:41 -0600

    Update Finehout_Talon_hw5_gridsearch.py

--- Finehout_Talon_hw5_gridsearch.py
+++ Finehout_Talon_hw5_gridsearch.py
@@ -2,36 +2,64 @@ from sklearn.model_selection import GridSearchCV
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.preprocessing import LabelEncoder
 from sklearn.linear_model import LogisticRegression
-
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, f1_score
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+
 import pandas as pd
 import numpy as np
 
 
-
-df = pd.read_csv("reduced_rotten_tomatoes_movie_reviews.csv")
-documents = df["reviewText"][0:500]
-true_sentiment = df['scoreSentiment'][0:500]
-#print("True Sentiment",true_sentiment)
+df = pd.read_csv("train.csv")
+documents = df["v2"][0:5000]
+true_sentiment = df['v1'][0:5000]
 
 bow = CountVectorizer()
-x = bow.fit_transform(documents)
 label_encoder = LabelEncoder()
+
+x = bow.fit_transform(documents)
 encoded = label_encoder.fit_transform(true_sentiment)
 
 dt = DecisionTreeClassifier()
-lr = LogisticRegression()
+svm = SVC()
+rfc = RandomForestClassifier()
 
-parameters_dt = {'criterion':["gini","entropy"],'max_depth':[10,15,20]}
-parameters_lr = {'penalty':["l1","l2"],'solver':['liblinear']}
-cv_dt = GridSearchCV(dt,parameters_dt,verbose=2)
-cv_dt.fit(x,encoded)
-cv_lr = GridSearchCV(lr,parameters_lr,verbose=2)
-cv_lr.fit(x,encoded)
+parameters_dt = {'criterion':["gini","entropy"],'max_depth':[10,15,20,25,30,35,40]}
+parameters_svm = {'kernel': ['linear', 'rbf'], 'C': [0.1, 1, 10]}
+parameters_rfc = {'n_estimators': [50, 100, 200], 'max_depth': [10, 20, 30]}
 
-print(cv_dt.best_estimator_)
-print(cv_lr.best_params_)
+cv_dt = GridSearchCV(dt, parameters_dt, verbose=2)
+cv_dt.fit(x, encoded)
+
+cv_svm = GridSearchCV(svm, parameters_svm, verbose=2)
+cv_svm.fit(x, encoded)
+
+cv_rfc = GridSearchCV(rfc, parameters_rfc, verbose=2)
+cv_rfc.fit(x, encoded)
+
+
+print("Best parameters for Decision Tree Classifier:")
+print(cv_dt.best_params_)  # Best params for DT
+print()
+
+print("Best parameters for Support Vector Machine Classifier:")
+print(cv_svm.best_params_) # Best params for SVM
+print()
+
+print("Best parameters for Random Forest Classifier:")
+print(cv_rfc.best_params_) # Best params for RFC
+
+'''
+Best parameters for Decision Tree Classifier:
+{'criterion': 'gini', 'max_depth': 30}
+
+Best parameters for Support Vector Machine Classifier:
+{'C': 0.1, 'kernel': 'linear'}
+
+Best parameters for Random Forest Classifier:
+{'max_depth': 30, 'n_estimators': 100}
+'''
 
 
 
